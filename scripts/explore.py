5151f56dd4a996ca1c2aad6c82d32e47c6541b81
elisabeth-ms <emenende@pa.uc3m.es>
2018-05-28 16:41:10 +0200

    Algoritmo de exploracion, utilizando celda de la fronter mas cercana o una aleatoria.

--- scripts/explore.py
+++ scripts/explore.py
@@ -8,6 +8,8 @@ from geometry_msgs.msg import Twist
 from geometry_msgs.msg import PoseStamped
 from std_msgs.msg import Header
 import random
+from scipy.cluster.vq import vq, kmeans,whiten
+import tf
 class Explore:
     def __init__(self):
         rospy.Subscriber("map", OccupancyGrid, self.callback_map)
@@ -25,8 +27,13 @@ class Explore:
         self.rate = rospy.Rate(1.0)  # 10hz
         self.goal = PoseStamped()
         self.end = False
+        self.use_random_cell = False
+        self.current_x = 0
+        self.current_y = 0
         t = Twist()
         t.angular.z = 0.7
+        self.listener = tf.TransformListener()
+
         while not rospy.is_shutdown():
             if self.init:
                 for i in range(16):
@@ -35,6 +42,7 @@ class Explore:
                     if i == 15:
                         self.init = False
 
+            print "Current:",self.current_x,self.current_y
             rospy.spin()
 
 
@@ -67,28 +75,55 @@ class Explore:
                     self.goal_reached = True
                     print "Goal is free"
             if not self.init and self.goal_reached:
-                self.fronteirs=[]
-                for i in range(0, data.info.width):
-                    for j in range(0, data.info.height):
-                        if self.map[i][j]==-1 and self.check_neighbors(i,j) and not self.check_closer_obstacles(i,j):
-                            self.fronteirs.append([i,j])
-                    #print self.fronteirs
+                if self.use_random_cell:
+                    self.fronteirs = []
+                    for i in range(0, data.info.width):
+                        for j in range(0, data.info.height):
+                            if self.map[i][j] == -1 and self.check_neighbors(i, j) and not self.check_closer_obstacles(i, j):
+                                self.fronteirs.append([i, j])
+                else:
+                     (trans, rot) = self.listener.lookupTransform('/map', '/base_footprint', rospy.Time(0))
+                     self.current_x = trans[0]
+                     self.current_y = trans[1]
+                     current_cell_x = int((self.current_x-self.origin_x)/self.res)
+                     current_cell_y = int((self.current_y- self.origin_y)/ self.res)
+                     print "Current cell:",current_cell_x,current_cell_y
+                     self.fronteirs=[]
+                     dmin = 100000000
+                     for i in range(0, data.info.width):
+                         for j in range(0, data.info.height):
+                             if self.map[i][j] == -1 and self.check_neighbors(i, j) and not self.check_closer_obstacles(
+                                 i, j):
+                                 d = self.distance(current_cell_x,current_cell_y,i,j)
+                                 if d <=dmin:
+                                    if self.fronteirs:
+                                        self.fronteirs.pop(0)
+                                    self.fronteirs.append([i,j])
+                                    dmin = d
+
                 if not self.fronteirs:
                     self.end = True
                     self.goal_reached = True
                     print "Complete Area scanned"
                 else:
-                    index = random.randint(0,len(self.fronteirs)-1)
+                    print self.fronteirs
+                    if self.use_random_cell:
+                        index = random.randint(0, len(self.fronteirs) - 1)
+                    else:
+                        index = 0
                     print "Assigning new goal:"
-                    #print self.fronteirs[index]
+                    print self.fronteirs[index]
                     self.goal.header.frame_id = "map"
-                    self.goal.pose.position.x =self.fronteirs[index][0] * self.res + self.origin_x
-                    self.goal.pose.position.y =self.fronteirs[index][1]* self.res + self.origin_y
-                    print self.goal.pose.position.x,self.goal.pose.position.y
+                    self.goal.pose.position.x = self.fronteirs[index][0] * self.res + self.origin_x
+                    self.goal.pose.position.y = self.fronteirs[index][1] * self.res + self.origin_y
+                    print self.goal.pose.position.x, self.goal.pose.position.y
                     self.goal.pose.orientation.w = 1
                     self.pub_goal.publish(self.goal)
                     self.goal_reached = False
 
+
+    def distance(self, current_cell_x, current_cell_y,fronteir_cell_x,fronteir_cell_y):
+        return (current_cell_x - fronteir_cell_x) * (current_cell_x - fronteir_cell_x) + (current_cell_y - fronteir_cell_y) * (current_cell_y - fronteir_cell_y)
     def check_neighbors(self,x,y):
         #Check if a neighbor is free
         if x+1 >= len(self.map[0]) or y+1>= len(self.map) or x-1<=0 or y-1<=0:
